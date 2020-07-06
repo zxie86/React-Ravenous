c38aba8e59835d0da5005c9d96dec4ad395765f1
Kelly <zxie86@uwo.ca>
2020-07-06 13:33:10 -0600

    p4

---
+++ src/util/Yelp.js
@@ -0,0 +1,33 @@
+const apiKey='w-D7eW4t33j56yIhuzvzBeFa_2LboFnqfxvJGBsq1epv59-GRFVnVmk-AATuzy5bo6CDiOSb5kS4LSFkTxfU2jBUglsYDSo-rlgmHnekJ_-AG7vePVug5Ys1iVoDX3Yx';
+const Yelp = {
+    search(term, location, sortBy) {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      }).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+            
+          return jsonResponse.businesses.map(business => {
+              
+            return{
+                id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,}
+          });
+        }
+      });
+    }
+  };
+  
+  export default Yelp;
+  
